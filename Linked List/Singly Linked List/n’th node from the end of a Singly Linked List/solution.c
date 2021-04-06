feaf8aa0efaa9d262045f8520f7af05734331cce
Sudir Krishnaa RS <67383465+SudirKrishnaaRS@users.noreply.github.com>
2021-04-06 20:27:35 +0530

    Create solution.c

---
+++ Linked List/Singly Linked List/n’th node from the end of a Singly Linked List/solution.c
@@ -0,0 +1,62 @@
+// n’th node from the end of a Singly Linked List
+#include <stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+struct node *head,*nn,*old,*temp;
+
+void create()
+{
+    int n=0;
+    printf("Enter the number of nodes:");
+    scanf("%d",&n);
+    head=(struct node*)malloc(sizeof(struct node));
+    scanf("%d",&head->data);
+    head->next=NULL;
+    old=head;
+    for(int i=1;i<n;i++)
+    {
+        nn=(struct node*)malloc(sizeof(struct node));
+        scanf("%d",&nn->data);
+        nn->next=NULL;
+        old->next=nn;
+        old=old->next;
+    }
+}
+
+void display()
+{
+    int pos=0,len=0;
+    
+    //here we find the total count of nodes 
+    temp=head;
+    while(temp!=NULL)
+    {
+        len++;
+        temp=temp->next;
+    }
+    printf("Enter the pos to be found from last :");
+    scanf("%d",&pos);
+    int res_pos=len-pos;
+    
+//as we traversed the temp to last in above to find count so we reassign the temp again to head i.e)first position
+    temp=head;
+    for(int i=0;i<res_pos;i++)
+    {
+        temp=temp->next;
+    }
+    printf("%d",temp->data);
+}
+
+int main()
+{
+    create();
+    display();
+
+    return 0;
+}
+
